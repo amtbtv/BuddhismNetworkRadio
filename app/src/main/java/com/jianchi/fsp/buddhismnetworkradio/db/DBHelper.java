43ec2b918219cd179844530904c7b797bcb6bce4
amtbtv <amtb@mail.sfzd5.com>
2018-07-12 22:09:03 +0800

    1.直接从弘化网获取数据，感谢杜师兄帮助。2.修复部分手机视频不能全屏的问题。

--- app/src/main/java/com/jianchi/fsp/buddhismnetworkradio/db/DBHelper.java
+++ app/src/main/java/com/jianchi/fsp/buddhismnetworkradio/db/DBHelper.java
@@ -11,7 +11,7 @@ import android.database.sqlite.SQLiteOpenHelper;
 public class DBHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "rec.db";
-    private static final int DATABASE_VERSION = 2;
+    private static final int DATABASE_VERSION = 3;
     private static DBHelper helper;
     //private Context context;
 
